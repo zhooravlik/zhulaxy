9fe5c01558e4ede88243aeaee983e90b51a3b012
aliaksandr_zhurauliou <aliaksandr_zhurauliou@epam.com>
2015-04-15 13:03:01 +0300

    Initial commit w banner and htpasswd

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Lx Zhu private page is under construction.</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="/site.css" rel="stylesheet" />
+	</head>
+	<body>
+		<img src="/trippybckg.jpg" class="bckg-img" alt="Lx Zhu private page is under construction"/>
+	</body>
+</html>
