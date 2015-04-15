a2d15a7bddc4b7e8fd4b96848f886566f2dfc979
aliaksandr_zhurauliou <aliaksandr_zhurauliou@epam.com>
2015-04-15 17:28:54 +0300

    Initial preProduction

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 	<head>
 		<title>Lx Zhu private page is under construction.</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="/site.css" rel="stylesheet" />
+<!--		<link href="/site.css" rel="stylesheet" />-->
+        <link href="css/styles.css" rel="stylesheet" />
 	</head>
 	<body>
-		<img src="/trippybckg.jpg" class="bckg-img" alt="Lx Zhu private page is under construction"/>
+<!--		<img src="/trippybckg.jpg" class="bckg-img" alt="Lx Zhu private page is under construction"/>-->
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+        <div id='title'>
+          <span>
+            Lx Zhu
+          </span>
+          <br>
+          <span>
+            Portfolio. The beginning.
+          </span>
+        </div>
 	</body>
 </html>
