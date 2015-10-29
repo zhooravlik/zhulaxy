83565b377a603642f6f1a2c814129171ef009ab5
aliaksandr_zhurauliou <aliaksandr_zhurauliou@epam.com>
2015-10-29 14:20:20 +0300

    Initial commit

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <html>
-	<head>
-		<title>Lx Zhu private page is under construction.</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!--		<link href="/site.css" rel="stylesheet" />-->
-        <link href="css/styles.css" rel="stylesheet" />
-	</head>
-	<body>
-<!--		<img src="/trippybckg.jpg" class="bckg-img" alt="Lx Zhu private page is under construction"/>-->
-        <div id="stars-bckg">
-            <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-            <div id='stars'></div>
-            <div id='stars2'></div>
-            <div id='stars3'></div>
-            <div id='title' class="hinga">
-                <span>
-                    Lx Zhu
-                </span>
-                <br>
-                <span>
-                    Zhulaxy is preparing to the Big Bang
-                </span>
-                <br>
-                <span class="blinking">
-                    CV is under maintenance
-                </span>
-            </div>
-        </div>
-	</body>
+<head>
+    <title>Lx Zhu private page is under construction.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="css/styles.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/jquery.color.js"></script>
+    <script type="text/javascript" src="js/animation.js"></script>
+</head>
+<body>
+<div id="stars-bckg">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+    <div id="title" class="hinga">
+            <span>
+                Lx Zhu
+            </span>
+        <br>
+            <span>
+                Zhulaxy is preparing to the Big Bang
+            </span>
+        <br>
+            <span class="blinking">
+<!--            <span class="pulsator">-->
+                Maintenance...
+            </span>
+    </div>
+</div>
+</body>
 </html>
