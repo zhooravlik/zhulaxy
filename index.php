712296e7b07fb4f1397155e1134383a7e32ff0c7
Lx Zhu <zhooravlik@gmail.com>
2015-12-30 17:09:05 +0300

    Changed content to the vacation countdown

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <html>
 <head>
-    <title>Lx Zhu private page is under construction.</title>
+    <!--    <title>Lx Zhu private page is under construction.</title>-->
+    <title>Lx Zhu</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link href="css/styles.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="js/jquery.color.js"></script>
-    <script type="text/javascript" src="js/animation.js"></script>
+    <!--    <script type="text/javascript" src="js/animation.js"></script>-->
+    <script src="js/countdown.js"></script>
 </head>
 <body>
 <div id="stars-bckg">
@@ -16,18 +18,22 @@
     <div id='stars2'></div>
     <div id='stars3'></div>
     <div id="title" class="hinga">
-            <span class="pulsator">
-                Zhoolaxy
-            </span>
-        <br>
-            <span class="pulsator">
-                Preparing to the Big Bang
-            </span>
-        <br>
-        <br>
-            <p class="blinking">
-                Maintenance...
-            </p>
+        <!--            <span class="pulsator">-->
+        <!--                Zhoolaxy-->
+        <!--            </span>-->
+        <!--        <br>-->
+        <!--            <span class="pulsator">-->
+        <!--                Preparing to the Big Bang-->
+        <!--            </span>-->
+        <!--        <br>-->
+        <!--        <br>-->
+        <!--        <p class="blinking">-->
+        <!--            Maintenance...-->
+        <!--        </p>-->
+        <p class="blinking">
+            Gathering to the vacation...
+        </p>
+        <div id="CDT"></div>
     </div>
 </div>
 </body>
